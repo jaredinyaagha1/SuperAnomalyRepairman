739b67dd8f10af5d0a5698bb72d151987a42b203
orn <aurenarevalo@gmail.com>
2025-07-02 23:00:09 -0500

    Animations added for repair man. Changed to AnimationPlayer

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+##TODO If possible, change to animation tree for better animation handling
+
 # Movement parameters
 @export var move_speed := 200
 @export var jump_speed := -350
@@ -17,19 +19,30 @@ func _physics_process(delta):
 	apply_gravity(delta)
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		#$AnimationPlayer.play("jump")
 		velocity.y = jump_speed
 	move_and_slide()
 	if Input.is_action_just_pressed("switch_timeline_up"):
 		switch_timeline_up()
 	elif Input.is_action_just_pressed("switch_timeline_down"):
 		switch_timeline_down()
+	if velocity.y > 0:
+		$AnimationPlayer.play("fall")
+	elif velocity.y < 0:
+		$AnimationPlayer.play("jump")
 
 func handle_movement():
 	var direction := 0
 	if Input.is_action_pressed("move_left"):
+		$Sprite2D.flip_h = true
 		direction -= 1
 	if Input.is_action_pressed("move_right"):
+		$Sprite2D.flip_h = false
 		direction += 1
+	if direction and is_on_floor():
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.play("idle")
 	velocity.x = direction * move_speed
 
 func apply_gravity(delta):
@@ -40,10 +53,13 @@ func apply_gravity(delta):
 
 func switch_timeline_up():
 	current_era = (current_era + 1) % 3
+	$AnimationPlayer.play("time_shift")
 	emit_signal("timeline_switched", current_era)
 
 func switch_timeline_down():
 	current_era = (current_era - 1) % 3
 	if current_era < 0:
 		current_era = 2
+	$AnimationPlayer.play("time_shift")
+	
 	emit_signal("timeline_switched", current_era)
